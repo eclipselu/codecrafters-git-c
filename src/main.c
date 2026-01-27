e0c562a8075692600d8ce56c530dd1eeb8bc292e
Lu Dang <eclipselu@gmail.com>
2026-01-26 22:39:41 -0500

    organize code

--- src/main.c
+++ src/main.c
@@ -4,6 +4,30 @@
 #include <string.h>
 #include <sys/stat.h>
 
+static int init() {
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
+  fprintf(stderr, "Logs from your program will appear here!\n");
+
+  if (mkdir(".git", 0755) == -1 || mkdir(".git/objects", 0755) == -1 ||
+      mkdir(".git/refs", 0755) == -1) {
+    fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
+    return 1;
+  }
+
+  FILE *headFile = fopen(".git/HEAD", "w");
+  if (headFile == NULL) {
+    fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
+    return 1;
+  }
+  fprintf(headFile, "ref: refs/heads/main\n");
+  fclose(headFile);
+
+  printf("Initialized git directory\n");
+
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   // Disable output buffering
   setbuf(stdout, NULL);
@@ -17,25 +41,7 @@ int main(int argc, char *argv[]) {
   const char *command = argv[1];
 
   if (strcmp(command, "init") == 0) {
-    // You can use print statements as follows for debugging, they'll be visible
-    // when running tests.
-    fprintf(stderr, "Logs from your program will appear here!\n");
-
-    if (mkdir(".git", 0755) == -1 || mkdir(".git/objects", 0755) == -1 ||
-        mkdir(".git/refs", 0755) == -1) {
-      fprintf(stderr, "Failed to create directories: %s\n", strerror(errno));
-      return 1;
-    }
-
-    FILE *headFile = fopen(".git/HEAD", "w");
-    if (headFile == NULL) {
-      fprintf(stderr, "Failed to create .git/HEAD file: %s\n", strerror(errno));
-      return 1;
-    }
-    fprintf(headFile, "ref: refs/heads/main\n");
-    fclose(headFile);
-
-    printf("Initialized git directory\n");
+    return init();
   } else {
     fprintf(stderr, "Unknown command %s\n", command);
     return 1;
